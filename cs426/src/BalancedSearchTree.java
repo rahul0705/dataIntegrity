515519e2cc176a2ffa8650a9e7fb7429e5a85c00
nbachman <nbachman@sslab03.cs.purdue.edu>
2013-03-24 17:52:32 -0400

    made BST Class

--- cs426/src/BalancedSearchTree.java
+++ cs426/src/BalancedSearchTree.java
@@ -1,8 +1,48 @@
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BalancedSearchTree {
-	public BalancedSearchTree(File in){
-		
+	ArrayList<String> data;
+	String key;
+	String searchScheme;
+	
+	
+	public BalancedSearchTree(){
+		data = new ArrayList<String>();
+	}
+	
+	public void readData(String dataFile){
+		Scanner scan;
+		try {
+			scan = new Scanner(new File(dataFile));
+			while(scan.hasNext())
+				data.add(scan.nextLine());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readKey(String keyFile){
+		Scanner scan;
+		try {
+			scan = new Scanner(new File(keyFile));
+			key = scan.nextLine();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readTree(String treeFile){
+		Scanner scan;
+		try {
+			scan = new Scanner(new File(treeFile));
+			searchScheme = scan.nextLine();
+			while(scan.hasNext())
+				data.add(scan.nextLine());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ cs426/src/TestBalancedSearchTree.java
@@ -0,0 +1,17 @@
+
+public class TestBalancedSearchTree {
+	public static void main(String args[]){
+		BalancedSearchTree bst = new BalancedSearchTree();
+		for(int i = 0; i < args.length; i++){
+			if(args[i].equals("-t")){
+				bst.readTree(args[++i]);
+			}
+			if(args[i].equals("-k")){
+				bst.readKey(args[++i]);
+			}
+			if(args[i].equals("-d")){
+				bst.readData(args[++i]);
+			}
+		}
+	}
+}
