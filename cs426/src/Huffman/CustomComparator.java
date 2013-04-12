8df1541d81ad6cdacb2ecef23354157909c7c9ae
Rahul Mohandas <rahul0705@gmail.com>
2013-04-12 15:04:13 -0400

    packaged our code

--- cs426/src/Huffman/CustomComparator.java
+++ cs426/src/Huffman/CustomComparator.java
@@ -1,3 +1,4 @@
+package Huffman;
 import java.util.Comparator;
 
 public class CustomComparator implements Comparator<HuffmanNode>{
--- cs426/src/Huffman/Huffman.java
+++ cs426/src/Huffman/Huffman.java
@@ -1,3 +1,4 @@
+package Huffman;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
